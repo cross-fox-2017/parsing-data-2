418903c4c366944c14ee85ac3b7b2d853cdfb58e
Your Name <name@example.com>
2017-01-09 15:41:03 +0700

    Install require yml-json-csv

--- parser.js
+++ parser.js
@@ -1,34 +1,7 @@
 "use strict"
-
-class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
-}
-
-class PersonParser {
-
-  constructor(file) {
-    this.file = file
-    this.people = []
-  }
-
-  get people() {
-    // If we've already parsed the CSV file, don't parse it again.
-    if (this.people)
-      return this.people
-
-    // We've never called people before, now parse the CSV file
-    // and return an Array of Person objects here.  Save the
-    // Array in the this.people instance variable.
-  }
-
-  save() {}
-
-  save_as_yaml() {}
-
-  save_as_json() {}
-}
-
-var parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+var fs = require('fs')
+var yaml = require('write-yaml');
+var jsonfile = require('jsonfile')
+var csv = require('fast-csv')
+var dataPerson = [];
+var temp = "";
