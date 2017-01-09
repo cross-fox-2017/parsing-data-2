b8676e7410e35241fcccd58ecc45b5dfae772630
Your Name <name@example.com>
2017-01-09 15:42:25 +0700

    Create PerseonData

--- parser.js
+++ parser.js
@@ -5,3 +5,14 @@ var jsonfile = require('jsonfile')
 var csv = require('fast-csv')
 var dataPerson = [];
 var temp = "";
+
+class PersonData {
+    constructor(params) {
+        this.id = params['id']
+        this.first_name = params['first_name']
+        this.last_name = params['last_name']
+        this.email = params['email']
+        this.phone = params['phone']
+        this.created_at = params['created_at']
+    }
+}
